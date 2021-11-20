a585df04b03b004adb5ab688e7cc78292d145b29
Alex Marshall <alex.k.marshall83@gmail.com>
2021-11-20 11:19:53 +0000

    add desktop navigation

--- src/features/header/header.tsx
+++ src/features/header/header.tsx
@@ -11,19 +11,40 @@ import {
 
 import { Cart } from '..'
 import Image from 'next/image'
+import Link from 'next/link'
 import Logo from 'public/logo.svg'
 import { ReactNode } from 'react'
 import { sprinkles } from 'src/styles/sprinkles.css'
+
+const navigationLinks = [
+  { label: 'Collections', url: '/' },
+  { label: 'Men', url: '/' },
+  { label: 'Women', url: '/' },
+  { label: 'About', url: '/' },
+  { label: 'Contact', url: '/' },
+]
 
 export function Header(): JSX.Element {
   return (
     <header className={styles.header}>
       <Cluster space="xs" justify="space-between">
-        <Cluster space="xs">
-          <MediaQueryVisibility>
+        <Cluster space={{ all: 'xs', desktop: '2xl' }}>
+          <Box component="span" display={{ all: 'block', desktop: 'none' }}>
             <MobileNavigation />
-          </MediaQueryVisibility>
+          </Box>
           <Image src={Logo} alt="Sneakers" />
+
+          <Box component="nav" display={{ all: 'none', desktop: 'block' }}>
+            <Cluster component="ul" space="xl">
+              {navigationLinks.map((navItem) => (
+                <li key={navItem.label}>
+                  <Link href={navItem.url}>
+                    <a className={styles.navAnchor}>{navItem.label}</a>
+                  </Link>
+                </li>
+              ))}
+            </Cluster>
+          </Box>
         </Cluster>
 
         <Cluster space="xs">
@@ -35,11 +56,4 @@ export function Header(): JSX.Element {
       </Cluster>
     </header>
   )
-}
-
-function MediaQueryVisibility({ children }: { children: ReactNode }) {
-  const sprinklesClass = sprinkles({
-    display: { all: 'block', desktop: 'none' },
-  })
-  return <span className={sprinklesClass}>{children}</span>
 }
